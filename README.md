6308d7ac2739772180816605e9994530edd9e740
cyio <icaner@qq.com>
2017-12-25 22:50:41 -0500

    添加 telegram 通知支持

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # blockchain-monitor
 
 ## 功能
-监视指定 BCH/BTC 地址，有新收歀时，发一条微信通知
+监视指定 BCH/BTC 地址，有新收歀时，发一条通知，支持微信和 Telegram
 
 ## 安装
 ```shell
@@ -18,16 +18,20 @@ $ cp config.json.template config.json
   * `address_type`  bch/btc
   * `observed_address` 监视地址
   * `server_chan_key`  需要你配置 [Server酱](http://sc.ftqq.com/3.version)，并提到专属 key
+	* `telegram_bot_key` 和 `telegram_chat_id"`，请参考 Telegram Bot Api 文档设置
+	* 设置 key 的才会发送，一般根据使用偏好，设置一个即可
 
 ```
 {
   "address_type": "bch",
   "observed_address": "",
-  "server_chan_key": ""
+  "server_chan_key": "",
+	"telegram_bot_key": "",
+	"telegram_chat_id": ""
 }
 ```
 
 持久运行（需另行配置 pm2 或类似工具）
 ```
-pm2 start index.js --watch
+pm2 start index.js --watch --name 'blockchain-monitor'
 ```
--- index.js
+++ index.js
@@ -23,16 +23,20 @@ const app = {
       if (data.unconfirmed_received && data.last_tx !== lastTxId) { // debug 时，第一个条件改为相反
         lastTxId = data.last_tx
         const txDetail = await this.getTxDetail(data.last_tx)
-        const title = `收到 ${txDetail.value * 0.00000001} ${isBCH ? 'BCH' : 'BTC'}`
-        const desp = `> 创建时间  
-          ${this.formatDate(txDetail.time)}  
-          交易ID  
-          ${data.last_tx}  
-          [查看交易](https://m${isBCH ? 'bch' : ''}.btc.com/${data.last_tx})
+        const title = `收到 ${txDetail.value * 0.00000001} ${isBCH ? 'BCH' : 'BTC'}  `
+        const desp = `
+
+        创建时间 ${this.formatDate(txDetail.time)}  
+        [查看交易](https://m${isBCH ? 'bch' : ''}.btc.com/${data.last_tx})
         `
         console.log(desp)
 				// return		// debug 时，取消注释
-        this.serverChan(title, desp)
+        if (config.server_chan_key) {
+          this.serverChan(title, desp)
+        }
+        if (config.telegram_bot_key && config.telegram_chat_id) {
+          this.sendToTelegram(title + desp)
+        }
       }
     })
   },
@@ -47,7 +51,7 @@ const app = {
     return detail
   },
   serverChan(title, desp) {
-    return axios.post(`http://sc.ftqq.com/${config.serverChanKey}.send`,
+    return axios.post(`http://sc.ftqq.com/${config.serverchan_key}.send`,
       querystring.stringify({
         text: title,
         desp: desp
@@ -60,8 +64,20 @@ const app = {
         console.error(error);
       });
   },
+  sendToTelegram(text) {
+    const url = `https://api.telegram.org/bot${config.telegram_bot_key}/sendMessage?chat_id=${config.telegram_chat_id}&disable_web_page_preview=true&parse_mode=markdown`
+    axios.post(url, 
+      querystring.stringify({
+        text: text
+      }))
+      .then(res => {
+        if (res.data.ok) {
+          return console.log('telegram send success')
+        }
+      })
+  },
   formatDate(t) {
-		return (new Date(t * 1000)).toLocaleString('zh-CN')
+    return (new Date(t * 1000)).toLocaleString('zh-CN')
   }
 }
 
