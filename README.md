05c0d81b5b85e0a327c7a4faa12d09902fbe43a8
cyio <icaner@qq.com>
2018-07-12 17:36:09 +0800

    支持批量查询

--- README.md
+++ README.md
@@ -11,26 +11,26 @@ $ git clone https://github.com/cyio/block-monitor
 $ npm install
 ```
 ## 配置
-复制配置模板
+1. 复制配置模板
 ```shell
 $ cp config.json.template config.json
 ```
 配置说明
 
   * `address_type`  bch 或 btc
-  * `observed_address` 需要监视钱包地址，必填
   * `server_chan_key`  需要你配置 [Server酱](http://sc.ftqq.com/3.version)，并提到专属 key ，默认为空。配置方生效
   * `telegram_bot_key` 和 `telegram_chat_id"`，请参考 Telegram Bot Api 文档设置
 
 ```
 {
   "address_type": "bch",
-  "observed_address": "",
   "server_chan_key": "",
   "telegram_bot_key": "",
   "telegram_chat_id": ""
 }
 ```
+2. 创建文件`./node_modules/addresses.txt`
+一行一个地址，支持 legacy 和 cashAddress
 
 持久运行（需另行配置 pm2 或类似工具）
 ```
--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const axios = require('axios'),
- time = require("time"),
- querystring = require('querystring'),
- config = require('./config.json');
+  time = require("time"),
+  querystring = require('querystring'),
+  readline = require('readline'),
+  fs = require('fs'),
+  config = require('./config.json'),
+  bchaddr = require('bchaddrjs')
+  env = process.env.NODE_ENV || 'development';
 
 time.tzset("Asia/Shanghai");
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+let addresses = []
+const rl = readline.createInterface({
+  input: fs.createReadStream('./node_modules/addresses.txt'),
+})
+function collectAddressFromLine(line) {
+  if (line) {
+    let address = line.trim()
+    if (bchaddr.isCashAddress(address)) {
+      address = bchaddr.toLegacyAddress(address)
+    }
+    addresses.push(address)
+  }
+}
+rl.on('line', (line) => {
+  collectAddressFromLine(line)
+})
+rl.on('close', (line) => {
+  collectAddressFromLine(line)
+  console.log({addresses})
+  app.init()
+})
 
 const isBCH = config.address_type === 'bch'
 const API = `https://${isBCH ? 'bch-' : ''}chain.api.btc.com/v3`
@@ -13,32 +39,48 @@ let lastTxId = null   // 缓存最新交易ID
 
 const app = {
   init() {
-    this.checkAddress()   // 立刻执行一次
-    setInterval(() => this.checkAddress(), 10000)
+    this.checkAddress(addresses[0])   // 立刻执行一次
+    let addrStr
+    if (addresses.length > 1) {
+      addrStr = addresses.join(',')
+    } else {
+      addrStr = addresses
+    }
+    setInterval(() => this.checkAddress(addrStr), 10000)
   },
-  checkAddress() {
-    axios.get(`${API}/address/${config.observed_address}`).then(async res => {
-      const data = res.data.data
+  checkAddress(addrStr) {
+    console.log('check')
+    axios.get(`${API}/address/${addrStr}`).then(async res => {
+      let data = res.data.data
       if (!data) return console.error('地址无效')
-      if (data.unconfirmed_received && data.last_tx !== lastTxId) { // debug 时，第一个条件改为相反
-        lastTxId = data.last_tx
-        const txDetail = await this.getTxDetail(data.last_tx)
-        const title = `收到 ${txDetail.value * 0.00000001} ${isBCH ? 'BCH' : 'BTC'}  `
-        const desp = `
+      if (data.length) {
+        for (let itemData of data) {
+          this.checkBalance(data)
+        }
+      } else {
+        this.checkBalance(data)
+      }
+    })
+  },
+  async checkBalance(data) {
+    if (data.unconfirmed_received && data.last_tx !== lastTxId) { // debug 时，第一个条件改为相反
+      lastTxId = data.last_tx
+      const txDetail = await this.getTxDetail(data.last_tx)
+      const title = `收到 ${txDetail.value * 0.00000001} ${isBCH ? 'BCH' : 'BTC'}  `
+      const desp = `
 
         创建时间 ${this.formatDate(txDetail.time)}  
         [查看交易](https://m${isBCH ? 'bch' : ''}.btc.com/${data.last_tx})
         `
-        console.log(desp)
-        // return		// debug 时，取消注释
-        if (config.server_chan_key) {
-          this.serverChan(title, desp)
-        }
-        if (config.telegram_bot_key && config.telegram_chat_id) {
-          this.sendToTelegram(title + desp)
-        }
+      console.log(desp)
+      // return		// debug 时，取消注释
+      if (config.server_chan_key) {
+        this.serverChan(title, desp)
       }
-    })
+      if (config.telegram_bot_key && config.telegram_chat_id) {
+        this.sendToTelegram(title + desp)
+      }
+    }
   },
   async getTxDetail(id) {
     const detail = await axios.get(`${API}/tx/${id}`).then(res => {
@@ -81,6 +123,4 @@ const app = {
     return (new Date(t * 1000)).toLocaleString('zh-CN')
   }
 }
-
-app.init()
 
